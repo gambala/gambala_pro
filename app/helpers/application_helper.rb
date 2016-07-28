a57b701f94c641c21af9d77c3a6a5a2e951f19a9
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-07-28 15:45:57 +0300

    Make site responsive on iphone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def viewport_tag
+    params = %w(width=device-width
+                initial-scale=1
+                minimum-scale=1
+                maximum-scale=1
+                user-scalable=0)
+    content_tag :meta, nil, name: 'viewport', content: params.join(', ')
+  end
 end
