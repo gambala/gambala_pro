2c513f2b2d4b7fa69e81548d3b848ce39a6fbf60
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-08-01 03:26:56 +0300

    Install clearance gem and configs and migrate the database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = false
   config.active_record.migration_error = :page_load
