4381feb30a0a5179f77aaddf4d1af936d1223485
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-08-15 12:50:07 +0300

    Refactor link with external_url helper

---
+++ app/helpers/url_helper.rb
@@ -0,0 +1,8 @@
+module UrlHelper
+  def external_url(url)
+    return url if url =~ %r{^https://}
+    return url if url =~ %r{^http://}
+    return url if url =~ %r{^//}
+    "//#{url}"
+  end
+end
