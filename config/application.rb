430c19b70ac061429d7abacb19c55f0e3fef65d5
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-08-20 02:13:15 +0300

    Finish fix-form-builder

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,10 @@
 require_relative 'boot'
 require 'rails/all'
+require './lib/form_builders/bootstrap_form_builder'
 Bundler.require(*Rails.groups)
 
 module Gambala
   class Application < Rails::Application
-    config.autoload_paths += %W(
-      #{config.root}/lib/form_builders
-    )
     config.action_view.default_form_builder = 'BootstrapFormBuilder'
     config.encoding = 'utf-8'
     config.i18n.available_locales = [:ru, :en]
