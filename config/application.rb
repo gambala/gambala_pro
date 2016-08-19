a15506fa3e736785d797b1b7a9e80ec722795554
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-08-20 01:54:38 +0300

    Try to fix form builder bug in production

--- config/application.rb
+++ config/application.rb
@@ -4,10 +4,10 @@ Bundler.require(*Rails.groups)
 
 module Gambala
   class Application < Rails::Application
-    config.action_view.default_form_builder = 'BootstrapFormBuilder'
     config.autoload_paths += %W(
       #{config.root}/lib/form_builders
     )
+    config.action_view.default_form_builder = 'BootstrapFormBuilder'
     config.encoding = 'utf-8'
     config.i18n.available_locales = [:ru, :en]
     config.i18n.default_locale = :ru
