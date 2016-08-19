85be6a0b8be03ed9a87bf09aaa8387b0b2b7dfb4
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-08-19 22:12:01 +0300

    Create Hit model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819111120) do
+ActiveRecord::Schema.define(version: 20160819191039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hits", force: :cascade do |t|
+    t.string   "title"
+    t.string   "slug"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
