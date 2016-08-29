e44eee8560f9226639fc00277597a2e9d9fa42ca
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-08-29 11:26:46 +0300

    Add footer and stylize online messager

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+  helper_method :deployed_at
+
+  private
+
+  def deployed_at
+    File.atime("#{Rails.root}/REVISION").utc
+  end
 end
