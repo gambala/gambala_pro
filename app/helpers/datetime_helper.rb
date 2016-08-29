3e385aabd0724d0f0d483ca30e656ef9a9035d3e
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-08-29 12:36:45 +0300

    Show hours in 24-hour system

--- app/helpers/datetime_helper.rb
+++ app/helpers/datetime_helper.rb
@@ -1,13 +1,13 @@
 module DatetimeHelper
   def human_date(datetime)
-    content_tag :span, datetime.strftime('%F %I:%M'), class: 'moment-date'
+    content_tag :span, datetime.strftime('%F %H:%M'), class: 'moment-date'
   end
 
   def human_datetime(datetime)
-    content_tag :span, datetime.strftime('%F %I:%M'), class: 'moment-datetime'
+    content_tag :span, datetime.strftime('%F %H:%M'), class: 'moment-datetime'
   end
 
   def human_time(datetime)
-    content_tag :span, datetime.strftime('%I:%M'), class: 'moment-time'
+    content_tag :span, datetime.strftime('%H:%M'), class: 'moment-time'
   end
 end
