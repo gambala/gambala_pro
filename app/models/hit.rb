7793d3f2fadaf11f9b2b04751dd9d2d5ff5b44a1
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-08-31 17:13:02 +0300

    Add annotation retrieving from hit.body

--- app/models/hit.rb
+++ app/models/hit.rb
@@ -31,6 +31,11 @@ class Hit < ApplicationRecord
     @cover = first_image_from_body
   end
 
+  def annotation
+    return @annotation if defined? @annotation
+    @annotation = annotation_from_body
+  end
+
   def happened_at_db_date
     happened_at.to_date.to_s(:db)
   end
@@ -53,9 +58,22 @@ class Hit < ApplicationRecord
 
   private
 
+  def annotation_from_body
+    body_before_page_break || body
+  end
+
+  def body_before_page_break
+    return unless page_break_position
+    body[0..page_break_position - 1]
+  end
+
   def first_image_from_body
     match = body.match(/src="([^"]+)"/)
     return unless match.present?
     match[1]
+  end
+
+  def page_break_position
+    @page_break_position ||= body.index('<div style="page-break-after: always">')
   end
 end
