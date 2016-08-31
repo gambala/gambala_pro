730d1c8db7dfb1a22d0838e89c4278340b5b3ac0
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-08-31 04:10:57 +0300

    Compile ckeditor plugins assets too

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,5 +3,6 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(
   ckeditor/config.js
   ckeditor/filebrowser/*
+  ckeditor/plugins/*
   ckeditor/skins/moonocolor/*
 )
