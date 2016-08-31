e5f7522a74a3ca7315fe031e0f0b0f3a54138f51
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-08-31 03:07:23 +0300

    Precompile ckeditor filebrowser images too

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,5 +2,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w(
   ckeditor/config.js
+  ckeditor/filebrowser/*
   ckeditor/skins/moonocolor/*
 )
