d9af8030ba22c8d98e1b3802f8cd68036f5f4728
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-09-03 14:09:48 +0300

    Add pages for projects

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       namespace :admin do
         resources :guides
         resources :hits
+        resources :projects
 
         root to: 'welcome#index'
       end
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
 
     resources :guides, only: [:index, :show]
     resources :hits, only: [:index, :show]
+    resources :projects, only: [:index, :show]
     root to: 'welcome#index'
   end
 end
