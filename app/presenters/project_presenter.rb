1c51b5e5dec98f7f3796c28acc1f469a184d45de
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-09-06 16:05:13 +0300

    Show project hits with groups

--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -1,6 +1,11 @@
 class ProjectPresenter < ApplicationPresenter
   presents :project
 
+  def hit_groups
+    return unless project.hits.any?
+    @hit_groups ||= project.hits.order(happened_at: :desc).group_by(&:happened_at_db_date)
+  end
+
   def mockup_section_class
     @mockup_section_class ||= [page_mockup_class,
                                project.page_cover_class].join(' ')
