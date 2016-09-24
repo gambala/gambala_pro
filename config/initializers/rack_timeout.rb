e0aa5cd909950678c73872da5eb1a7f5ea8bbf41
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2016-09-24 19:00:04 +0300

    Remove rack timeout gem

--- config/initializers/rack_timeout.rb
+++
@@ -1,5 +0,0 @@
-Rack::Timeout.service_timeout = if Rails.env.production?
-                                  15  # seconds
-                                else
-                                  120  # seconds
-                                end
