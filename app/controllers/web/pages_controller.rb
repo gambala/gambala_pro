4f726bda0bcaae1fc4002b2def77d6836c956a7d
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-01-27 21:00:51 +0300

    Add empty landing page with breadcrumbs

---
+++ app/controllers/web/pages_controller.rb
@@ -0,0 +1,21 @@
+module Web
+  class PagesController < Web::ApplicationController
+    def show
+      if partial_name_correct?
+        render partial_name
+      else
+        redirect_to :root, status: 404
+      end
+    end
+
+    private
+
+    def partial_name
+      params[:id].underscore
+    end
+
+    def partial_name_correct?
+      %w(frontend).include? params[:id]
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
 
   scope module: :web do
+    resources :pages, path: '', only: :show
+
     root to: 'welcome#index'
     get '*path', to: redirect('/')
   end
