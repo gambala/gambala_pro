8ffc2553d18b688707cc17f79e24ed5f51e45ee7
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-01-31 23:07:08 +0300

    Add direct link to google.docs resume

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
 
   scope module: :web do
-    resources :pages, path: '', only: :show
+    get 'resume', to: redirect('https://docs.google.com/document/d/1qKAryiJ9vbV-4xwfq94_RheYCUWG4y_1UNL-qYmGOpk/edit?usp=site')
 
+    resources :pages, path: '', only: :show
     root to: 'welcome#index'
     get '*path', to: redirect('/')
   end
