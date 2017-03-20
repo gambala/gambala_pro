fa4a170355e4772521f413e5638a0745b6693543
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-03-20 16:56:52 +0300

    Try get bookmarks route clearly

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   scope module: :web do
     get 'resume', to: redirect('https://docs.google.com/document/d/1qKAryiJ9vbV-4xwfq94_RheYCUWG4y_1UNL-qYmGOpk/edit?usp=site')
 
+    get 'bookmarks', to: redirect('/bookmarks.html')
     resources :pages, path: '', only: :show
     root to: 'welcome#index'
   end
