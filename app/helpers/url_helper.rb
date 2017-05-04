c635e9eca9fc3e25016e7c6d9a07a39a9f6dd89b
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-05-04 13:08:13 +0300

    Use match for regexp instead of perl =~ construction

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 module UrlHelper
   def external_url(url)
-    return url if url =~ %r{^https://}
-    return url if url =~ %r{^http://}
-    return url if url =~ %r{^//}
+    return url if url.match?(%r{^https://})
+    return url if url.match?(%r{^http://})
+    return url if url.match?(%r{^//})
     "//#{url}"
   end
 end
