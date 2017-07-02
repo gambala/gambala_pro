6b2715f824c909ab12cf4538199bed373ed44343
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-07-02 14:25:52 +0300

    Remove frontend page and the whole pages resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     get 'resume', to: redirect('https://docs.google.com/document/d/1qKAryiJ9vbV-4xwfq94_RheYCUWG4y_1UNL-qYmGOpk/edit?usp=site')
 
     get 'bookmarks', to: redirect('/bookmarks.html')
-    resources :pages, path: '', only: :show
     root to: 'welcome#index'
   end
 end
