3bc719a686b66e5a9b18f0e2624412187007d010
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2017-07-24 20:14:44 +0300

    Add locale scope into routes and param into urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,19 @@
 
 class ApplicationController < ActionController::Base
   helper_method :deployed_at
+  before_action :set_locale
 
   private
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   def deployed_at
     File.atime(Rails.root.join('REVISION')).utc
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope module: :web do
-    get 'bookmarks', to: redirect('/bookmarks.html')
-    get 'resume', to: redirect('https://docs.google.com/document/d/1qKAryiJ9vbV-4xwfq94_RheYCUWG4y_1UNL-qYmGOpk/edit?usp=site')
-    root to: 'welcome#index'
-    get '*unmatched_route', to: 'errors#not_found'
+  scope '/:locale', locale: /#{I18n.available_locales.join('|')}/ do
+    scope module: :web do
+      get 'bookmarks', to: redirect('/bookmarks.html')
+      get 'resume', to: redirect('https://docs.google.com/document/d/1qKAryiJ9vbV-4xwfq94_RheYCUWG4y_1UNL-qYmGOpk/edit?usp=site')
+      root to: 'welcome#index'
+      get '*unmatched_route', to: 'errors#not_found'
+    end
   end
+
+  get '/*path', to: redirect("/#{I18n.default_locale}/%{path}")
+  root to: redirect("/#{I18n.default_locale}")
 end
