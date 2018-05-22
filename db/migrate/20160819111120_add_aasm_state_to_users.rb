7dcdb0a623f069895c92a6fa529734327d0477a9
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2018-05-22 23:03:29 +0300

    User rails version in all migrations

--- db/migrate/20160819111120_add_aasm_state_to_users.rb
+++ db/migrate/20160819111120_add_aasm_state_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddAasmStateToUsers < ActiveRecord::Migration
+class AddAasmStateToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :aasm_state, :string
   end
