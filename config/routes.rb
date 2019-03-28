a9b0c7f9e9c258ce235126657b859c371ddb509e
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2019-03-28 13:13:28 +0300

    Add posts-t html page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   scope '/:locale', locale: /#{I18n.available_locales.join('|')}/ do
     scope module: :web do
       get 'bookmarks', to: redirect('/bookmarks.html')
+      get 'posts-t', to: redirect('/posts-t.html')
       get 'resume', to: 'redirects#resume'
       root to: 'welcome#index'
       get '*unmatched_route', to: 'errors#not_found'
