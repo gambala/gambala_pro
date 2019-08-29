d1fda78b07fda44a2936c4f7b5bccf76e5c5b1e6
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2019-08-29 16:50:01 +0300

    Move all social buttons to top section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
   def fas_icon(icon)
     content_tag :i, nil, class: "fas fa-#{icon}"
   end
+
+  def social_link_to(icon, href)
+    link_to fa_icon(icon), href, target: '_blank', rel: 'noopener',
+            class: 'grid__item button button_30px button_white'
+  end
 end
