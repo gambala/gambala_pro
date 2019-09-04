a92258fae435ca90ff5bac07e42f5f6b0bd0a9d0
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2019-09-04 18:00:49 +0300

    Recolor public page

--- app/javascript/controllers/soda_js_controller.js
+++ app/javascript/controllers/soda_js_controller.js
@@ -2,6 +2,7 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
   connect() {
+    if (!this.element.querySelector('.soda-layout__burger')) return;
     if (!this.element.querySelector('.soda-aside')) {
       this.element.querySelector('.soda-layout__burger').classList.add('d_n');
     }
